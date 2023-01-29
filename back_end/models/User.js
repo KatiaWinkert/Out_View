a57d3426b3fb76b6fb12dad4883eedaed3e87f30
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-28 23:19:02 -0300

    User.js
    model criado para o usuario para inclusão de dados.
    25/01/2023 23:18

---
+++ back_end/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new Schema(
+  {
+    name: String,
+    emali: String,
+    password: String,
+    profileImage: String,
+    bio: String,
+  },
+  {
+    //marca a data em que o usuario vai ser criado ou atualizado. é interessando para sistema que dependende da data
+    //para alguma coisa.
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+module.export = User
