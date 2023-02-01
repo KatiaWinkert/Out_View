a8eb4a51bc9976d570229067061a25b7f97e166f
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-31 21:48:30 -0300

    UserController
    Validação do tolken

--- back_end/controllers/UserController.js
+++ back_end/controllers/UserController.js
@@ -1,13 +1,22 @@
-const User = require("../models/User")
+const User = require('../models/User')
 
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const jwtSecret = process.env.JWT_SECRET;
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+const jwtSecret = process.env.JWT_SECRET
 
-//Gerando o token do usuario (função auxiliar que ajuda gerar o token)
-const generateToken = (id) =>{
-    return jwt.sign({id}, jwtSecret, {
-        expiresIn: "7d",
-    })
+// Generate user token
+const generateToken = (id) => {
+  return jwt.sign({ id }, jwtSecret, {
+    expiresIn: '7d',
+  })
+}
+
+// Register user and sign in
+const register = async(req, res) => {
+  res.send('Registro')
+}
+
+module.exports = {
+  register,
 }
