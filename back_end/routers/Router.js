47e91b6f31a8d84d4ca3cf8f7cb87e0c49c56d1a
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-31 21:49:02 -0300

    Router
    construindo rotas
    31/01/2023 21:48

--- back_end/routers/Router.js
+++ back_end/routers/Router.js
@@ -1,10 +1,11 @@
-const express = require("express")
+const express = require('express')
 const router = express()
 
-//test route
+router.use('/api/users', require('./UserRoutes'))
 
-router.get("/", (req, res) => {
-    res.send("Api Working!")
+// test route
+router.get('/', (req, res) => {
+  res.send('API Working!')
 })
 
-module.exports = router 
+module.exports = router
