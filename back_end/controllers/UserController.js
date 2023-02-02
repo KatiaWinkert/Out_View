30cfa41b029f9fc880ab470d8577354c2b017396
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-01 21:39:40 -0300

    UserController.js
    Código implementado para gerenciar senha, com esse código o próprio sistema cria uma senha de difícil acesso evitando assim para que a conta seja invadida.

--- back_end/controllers/UserController.js
+++ back_end/controllers/UserController.js
@@ -13,8 +13,41 @@ const generateToken = (id) => {
 }
 
 // Register user and sign in
-const register = async(req, res) => {
-  res.send('Registro')
+const register = async (req, res) => {
+  const { name, email, password } = req.body
+
+  //checando se o usuario existe:
+  const user = await User.findOne({ email })
+
+  if (user) {
+    res.status(422).json({ errors: ['Esse email já esta cadastrado!'] })
+    return
+  }
+
+  // Generate password hash  gerando senha string aleatoria para que ninguem acesse os dados de forma indevida
+  // o proprio sistema gera através desses codigos:
+  const salt = await bcrypt.genSalt()
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  //Criar usuario = Create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  })
+
+  //chegando usuario foi criado com sucesso, retorna o token : if user was create sucessfully, return the token:
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ['Houve um erro, por favor tente mais tarde!'] })
+    return
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  })
 }
 
 module.exports = {
