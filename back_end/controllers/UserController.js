0622adae29aa6ee822f062876a747fedcaea6eec
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-03 21:20:55 -0300

    UserController.js
    Login de usuario - Verificação de senha e email valido

--- back_end/controllers/UserController.js
+++ back_end/controllers/UserController.js
@@ -50,9 +50,35 @@ const register = async (req, res) => {
   })
 }
 
-//Sing user in 
-const login = (req, res) => {
-  res.send('Login')
+//Sing user in - concluindo o login 
+const login = async (req, res) => {
+
+  
+  const { email, password } = req.body
+
+  const user = await User.findOne({email})
+
+  // para checar se usuario existe - check if user exists
+  if(!user){
+    res.status(404).json({errors: ["Usuario Não encontrado!"]})
+    return
+  }
+
+  //Chacando se a senha são iguais = check if password matches 
+
+  if (!( await bcrypt.compare(password, user.password))){
+    res.status(422).json({errors: ["Senha invalida."]})
+    return
+  }
+
+  
+  //Retornando o usuario com o Token - Return user with token (vou retornar tambem a imagem de perfil do usuario) nesse 
+  //codiogo eu posso retornar outros dados tb  
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  })
 }
 
 module.exports = {
