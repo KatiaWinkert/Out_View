763194cd4ae470b417afabf1c635a7be1e9a48ba
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-03 23:08:32 -0300

    authGuard
    Validação de token e código para que seja barrado um possível ação de quebra de token

---
+++ back_end/middlewares/authGuard.js
@@ -0,0 +1,28 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET
+
+const authGuard = async (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split('')[1]
+
+  //Bearer yy32156y12156y46545d546
+  //checando se o headers tem um token - check if header has a token:
+
+  if (!token) return res.status(401).json({errors: ["Acesso negado!"]})
+
+  //checando se o token é valido - check toke is valid 
+    try {
+        
+        const verified = jwt.verify(token, jwt)
+
+    } catch (error) {
+        res.status(401).json({errors: ["Token inválido!"]})
+        
+    }
+
+
+    module.exports = authGuard
+
+
+}
