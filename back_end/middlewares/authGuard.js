bf78e3dda7b17741d020d2ad8f112c751c644658
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-04 00:13:00 -0300

    authGuard.js
    Corrigindo bug do postaman (atualizando token invalido)

--- back_end/middlewares/authGuard.js
+++ back_end/middlewares/authGuard.js
@@ -4,25 +4,22 @@ const jwtSecret = process.env.JWT_SECRET
 
 const authGuard = async (req, res, next) => {
   const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split('')[1]
+  const token = authHeader && authHeader.split(' ')[1]
 
   //Bearer yy32156y12156y46545d546
   //checando se o headers tem um token - check if header has a token:
 
-  if (!token) return res.status(401).json({errors: ["Acesso negado!"]})
+  if (!token) return res.status(401).json({ errors: ['Acesso negado!'] })
 
-  //checando se o token é valido - check toke is valid 
-    try {
-        
-        const verified = jwt.verify(token, jwt)
+  //checando se o token é valido - check toke is valid
+  try {
+    const verified = jwt.verify(token, jwtSecret)
 
-    } catch (error) {
-        res.status(401).json({errors: ["Token inválido!"]})
-        
-    }
-
-
-    module.exports = authGuard
-
-
+    req.user = await User.findById(verified.id).select('-password')
+    next()
+  } catch (error) {
+    res.status(401).json({ errors: ['Token inválido!'] })
+  }
 }
+
+module.exports = authGuard
