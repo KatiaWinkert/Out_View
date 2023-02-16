132bb029bdcd2faec5f284557e5b1d76019e3498
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-16 00:05:54 -0300

    UserController
    Resgatando usuario pelo id

--- back_end/controllers/UserController.js
+++ back_end/controllers/UserController.js
@@ -125,9 +125,31 @@ const update = async (req, res) => {
   res.status(200).json(user)
 }
 
+//Resgatando usuario pelo id - Get user by Id
+const getUserByID = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+      '-password'
+    )
+    //checando se o usuario existe - check if user exists
+    if (!user) {
+      res.status(404).json({ error: ['Usuario não encontrado!'] })
+      return
+    }
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({ error: ['Usuario não encontrado!'] })
+    return
+  }
+}
+
 module.exports = {
   register,
   login,
   getCurrentUser,
   update,
+  getUserByID,
 }
--- back_end/routers/UserRoutes.js
+++ back_end/routers/UserRoutes.js
@@ -7,6 +7,7 @@ const {
   login,
   getCurrentUser,
   update,
+  getUserByID,
 } = require('../controllers/UserController')
 
 //Middlewares
@@ -31,5 +32,6 @@ router.put(
   imageUpload.single('profileImage'),
   update
 )
+router.get('/:id', getUserByID)
 
 module.exports = router
