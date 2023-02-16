cbf14bab7f103a8cede73c40cbec01f69c6378fe
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-15 23:28:19 -0300

    Usercontroller
    Atualizando 15/02/2023

--- back_end/controllers/UserController.js
+++ back_end/controllers/UserController.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
+const { default: mongoose } = require('mongoose')
 
 const jwtSecret = process.env.JWT_SECRET
 
