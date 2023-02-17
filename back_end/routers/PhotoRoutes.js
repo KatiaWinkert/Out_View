3219f7d0cb3e617b65b663fa26e2aaf58ef5c3b4
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-16 23:28:36 -0300

    PhotoRoutes
    incluindo rotas

--- back_end/routers/PhotoRoutes.js
+++ back_end/routers/PhotoRoutes.js
@@ -2,13 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 //Controller
+const { insertPhoto } = require('../controllers/PhotoController')
 
 //Middlewares
 const { photoInsertValidation } = require('../middlewares/photoValidation')
 const authGuard = require('../middlewares/authGuard')
 const validate = require('../middlewares/handleValidaition')
-
+const { imageUpload } = require('../middlewares/imageUpload')
 
 //Routes
+router.post(
+  '/',
+  authGuard,
+  imageUpload.single('image'),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+)
 
 module.exports = router
