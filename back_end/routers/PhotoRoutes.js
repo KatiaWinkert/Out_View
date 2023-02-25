bd329eb24b8166a5404d1b958e5b512d68b62168
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-25 00:39:30 -0300

    Photo Router
    implementando caminhos rotas para resgate de fotos por id

--- back_end/routers/PhotoRoutes.js
+++ back_end/routers/PhotoRoutes.js
@@ -7,6 +7,7 @@ const {
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
+  getPhotoById,
 } = require('../controllers/PhotoController')
 
 //Middlewares
@@ -27,6 +28,7 @@ router.post(
 router.delete('/:id', authGuard, deletePhoto)
 router.get('/', authGuard, getAllPhotos)
 router.get('/user/:id', authGuard, getUserPhotos)
+router.get('/:id', authGuard, getPhotoById)
 
 
 
