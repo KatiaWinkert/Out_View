aac7e46f7aee03dcefa11dda79edd7a332496084
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-25 00:10:18 -0300

    authGuard
    Atualização 25/02/2023 00:10

--- back_end/middlewares/authGuard.js
+++ back_end/middlewares/authGuard.js
@@ -17,7 +17,7 @@ const authGuard = async (req, res, next) => {
 
     req.user = await User.findById(verified.id).select('-password')
     next()
-  } catch (err) {
+  } catch (error) {
     res.status(401).json({ errors: ['Token inválido!'] })
   }
 }
