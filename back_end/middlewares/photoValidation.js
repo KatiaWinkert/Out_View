7ab56b30fca9e7764274d3272097d4468f7416b0
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-26 01:18:47 -0300

    photo Validation
    Validação de foto

--- back_end/middlewares/photoValidation.js
+++ back_end/middlewares/photoValidation.js
@@ -19,6 +19,18 @@ const photoInsertValidation = () => {
   ]
 }
 
+const photoUpdateValidation = () => {
+  return [
+    body('title')
+      .optional()
+      .isString()
+      .withMessage('O titulo é obrigatorio!')
+      .isLength({ min: 3 })
+      .withMessage('O titulo precisa ter no minimo 3 caracteres!'),
+  ]
+}
+
 module.exports = {
   photoInsertValidation,
+  photoUpdateValidation,
 }
