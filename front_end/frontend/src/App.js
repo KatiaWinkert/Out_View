419fa7f84f0c9cdf62d4450a255c06177cb135df
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-26 20:00:31 -0300

    App.js (Rotas)
    importações: pacotes essenciais para as Rotas, Css e paginas.
    inclusões de rotas no bloco principal do componente app.js.
    26/02/2023 20:00

--- front_end/frontend/src/App.js
+++ front_end/frontend/src/App.js
@@ -1,12 +1,24 @@
-
-import './App.css';
+//Router - Rotas
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+//css
+import './App.css'
+//pages
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
 
 function App() {
   return (
     <div className="App">
-     <h1>Out View</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
