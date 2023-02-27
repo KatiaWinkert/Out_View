c1700987f08579bc92792932640c936ac53f5e9c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-26 23:15:25 -0300

    Register.js
    Criando formulário para cadastro de usuário.

--- front_end/frontend/src/pages/Auth/Register.js
+++ front_end/frontend/src/pages/Auth/Register.js
@@ -1,7 +1,30 @@
 import './Auth.css'
+//components
+import { Link } from 'react-router-dom'
+//Hooks
+import { useState, useEffect } from 'react'
 
 const Register = () => {
-  return <div>Register</div>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <h2>Out View</h2>
+      <p className="subtitle">Criar uma conta</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem cadastrado? <Link to="/login">Clique aqui!</Link>
+      </p>
+    </div>
+  )
 }
 
 export default Register
