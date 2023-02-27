5eff36653404f6b16d91674d73aca6d6946f66c1
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-27 15:54:02 -0300

    Store,js
    importando authReducer
    identificando auth como authReducer.

--- front_end/frontend/src/store.js
+++ front_end/frontend/src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slices/authSlices'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 })
