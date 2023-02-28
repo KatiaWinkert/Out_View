7792ed24b746a7aefd4b6c2a01eaebafce351080
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-28 20:51:35 -0300

    authService.js
    Regularizando código com bug ( , depois da api foi retirada e acrescentado o sinal de +)

--- front_end/frontend/src/services/authService.js
+++ front_end/frontend/src/services/authService.js
@@ -6,14 +6,13 @@ const register = async (data) => {
   const config = requestConfig('POST', data)
 
   try {
-    const res = await fetch(api, '/users/register', config)
+    const res = await fetch(api +'/users/register', config)
       .then((res) => res.json())
       .catch((err) => err)
 
     if (res) {
       localStorage.setItem('user', JSON.stringify(res))
     }
-    
   } catch (error) {
     console.log(error)
   }
