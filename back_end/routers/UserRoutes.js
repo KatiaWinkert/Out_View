16d636d2d2de03c7caa8678a3723abb629375200
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-02-28 20:49:53 -0300

    Routes.js
    Atualizado: 28/02/2023 20:50

--- back_end/routers/UserRoutes.js
+++ back_end/routers/UserRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router()
 // Controller
 const {
   register,
-  login,
   getCurrentUser,
+  login,
   update,
   getUserByID,
 } = require('../controllers/UserController')
@@ -22,8 +22,8 @@ const { imageUpload } = require('../middlewares/imageUpload')
 
 // Routes
 router.post('/register', userCreateValidation(), validate, register)
-router.post('/login', loginValidation(), validate, login)
 router.get('/profile', authGuard, getCurrentUser)
+router.post('/login', loginValidation(), validate, login)
 router.put(
   '/',
   authGuard,
