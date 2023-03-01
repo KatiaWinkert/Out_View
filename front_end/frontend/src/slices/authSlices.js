ebc773476222f2f52d212f7f869dc2788e0646c0
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-01 20:26:07 -0300

    authSlice
    exportar logout de service para slice, implementar função logout

--- front_end/frontend/src/slices/authSlices.js
+++ front_end/frontend/src/slices/authSlices.js
@@ -26,6 +26,10 @@ export const register = createAsyncThunk(
   }
 )
 
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -53,6 +57,12 @@ export const authSlice = createSlice({
       .addCase(register.rejected, (state, action) => {
         state.loading = false
         state.error = action.payload
+        state.user = null
+      })
+      .addCase(logout.fulfilled, (state, action) => {
+        state.loading = false
+        state.success = true
+        state.error = null
         state.user = null
       })
   },
