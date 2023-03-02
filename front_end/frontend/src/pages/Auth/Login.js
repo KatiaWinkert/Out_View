4594758a575e09533872675398ff764f671b02e4
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-02 08:07:13 -0300

    Login.js
    importação de redux ( implementação de login para usuario)

--- front_end/frontend/src/pages/Auth/Login.js
+++ front_end/frontend/src/pages/Auth/Login.js
@@ -9,14 +9,30 @@ import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 // Redux
+import { login, reset } from '../../slices/authSlices'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const dispatch = useDispatch()
+
+  const { loading, error } = useSelector((state) => state.auth)
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = {
+      email,
+      password,
+    }
+    dispatch(login(user))
   }
+
+  // clean all auth state: 
+  useEffect(() =>{
+    dispatch(reset())
+  }, dispatch) 
 
   return (
     <div id="login">
@@ -35,7 +51,9 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password || ''}
         />
-        <input type="submit" value="Entrar" />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
       </form>
       <p>
         Não tem uma conta? <Link to="/register">clique aqui.</Link>
