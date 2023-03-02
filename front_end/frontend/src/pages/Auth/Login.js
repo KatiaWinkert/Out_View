fa88273ab7ec7e83b418260079e8364fec0755d3
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-01 21:16:12 -0300

    Login.js
    Criando formulário de login

--- front_end/frontend/src/pages/Auth/Login.js
+++ front_end/frontend/src/pages/Auth/Login.js
@@ -1,7 +1,47 @@
 import './Auth.css'
 
+//components
+import { Link } from 'react-router-dom'
+import Message from '../../components/Message'
+
+// Hooks
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+// Redux
+
 const Login = () => {
-  return <div>Login</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div id="login">
+      <h2>Out View</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ''}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ''}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">clique aqui.</Link>
+      </p>
+    </div>
+  )
 }
 
 export default Login
