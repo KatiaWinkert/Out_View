6adf7af6b47cac5bc89ff76fe9da426b25149eb2
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-04 21:13:50 -0300

    :construction: Back End
    Atualizando 04/03/2023

--- back_end/middlewares/userValidations.js
+++ back_end/middlewares/userValidations.js
@@ -47,11 +47,11 @@ const userUpdateValidation = () => {
     body('name')
       .optional()
       .isLength({ min: 3 })
-      .withMessage('O nome precisa ter no mínimo 3 caracteres!'),
+      .withMessage('O nome precisa ter no mínimo 3 caracteres.'),
     body('password')
       .optional()
       .isLength({ min: 5 })
-      .withMessage('A senha precisa ter no mínimo 5 caracteres!'),
+      .withMessage('A senha precisa ter no mínimo 5 caracteres.'),
   ]
 }
 
