ea7e920da206288790f81b1f6a1a0fc6e4fdcd7d
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-04 22:43:06 -0300

    :bug: authService
    bug

--- front_end/frontend/src/services/authService.js
+++ front_end/frontend/src/services/authService.js
@@ -29,7 +29,7 @@ const login = async (data) => {
   const config = requestConfig('POST', data)
 
   try {
-    const res = await fetch(api + '/users/register', config)
+    const res = await fetch(api + '/users/login', config)
     .then((res) =>
       res.json()
       .catch((err) => err)
