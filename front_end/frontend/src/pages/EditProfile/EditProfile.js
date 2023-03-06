3538ac5d370e275d7513e788f4200fa1a019deb3
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-06 11:55:30 -0300

    :sparkles: Edit Profile
    implementação (preenchendo formulário de edição com dados do usuário)

--- front_end/frontend/src/pages/EditProfile/EditProfile.js
+++ front_end/frontend/src/pages/EditProfile/EditProfile.js
@@ -1,6 +1,29 @@
 import './EditProfile.css'
+import  { uploads } from "../../utils/config"
+
+//hooks 
+import { useEffect, useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+
+//redux
+import {profile, resetMessage } from "../../slices/userSlice"
+
+//components 
+import Message from "../../components/Message"
 
 const EditProfile = () => {
+
+    const dispatch = useDispatch()
+
+    const {user, message, error, loading } = useSelector((state) => state.user)
+
+    //state 
+
+    //load user data 
+    useEffect(()=> {
+      dispatch(profile())
+    }, [dispatch])
+
   const handleSubmit = (e) => {
     e.preventDefault()
   }
