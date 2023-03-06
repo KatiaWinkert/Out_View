dbc8d031ccd280b5aa0a0058efca9c5f0d5e20ac
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-06 00:52:54 -0300

    :sparkles: userSlice
    Slice de perfil de usuário

--- front_end/frontend/src/slices/userSlice.js
+++ front_end/frontend/src/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import userService from '../services/userService'
 
 const initialState = {
   user: {},
@@ -8,7 +9,20 @@ const initialState = {
   message: null,
 }
 
-//funções:
+//funções: get user details
+
+export const profile = createAsyncThunk(
+  "user/profile", 
+  async(user, thunkAPI) => {
+
+      const token = thunkAPI.getState().auth.user.token
+
+      const data = await userService.profile(user, token)
+  
+    return data
+    }
+) 
+
 
 export const userSlice = createSlice({
   name: 'user',
@@ -18,6 +32,20 @@ export const userSlice = createSlice({
       state.message = null
     },
   },
+  extraReducers: (builder) => {
+     builder
+       .addCase(profile.pending, (state) => {
+         state.loading = true
+         state.error = null
+       })
+       .addCase(profile.fulfilled, (state, action) => {
+         state.loading = false
+         state.success = true
+         state.error = null
+         state.user = action.payload
+       })
+      
+  }
 })
 
 export const { resetMessage } = userSlice.actions
