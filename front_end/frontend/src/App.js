6cfdcc59053fa4d7828a269601e6b5b96eacc94c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-05 22:57:18 -0300

    :sparkles: App.js
    Inclusão de rota de edição de perfil edit profile

--- front_end/frontend/src/App.js
+++ front_end/frontend/src/App.js
@@ -5,12 +5,13 @@ import { useAuth } from './hooks/useAuth'
 //css
 import './App.css'
 //pages
-import  Home from './pages/Home/Home'
+import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 //components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import EditProfile from './pages/EditProfile/EditProfile'
 
 function App() {
   const { auth, loading } = useAuth()
@@ -28,6 +29,10 @@ function App() {
             <Route
               path="/"
               element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
