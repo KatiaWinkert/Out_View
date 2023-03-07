f23aa5ca2d13b5d054157189cba209a0d20e6c26
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-07 20:34:32 -0300

    :sparkles:
    Atualizando 07/03/2023

--- front_end/frontend/src/services/userService.js
+++ front_end/frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import { api, requestConfig } from '../utils/config'
 
-//get user details:
+// Get user details
 const profile = async (data, token) => {
   const config = requestConfig('GET', data, token)
 
@@ -8,32 +8,47 @@ const profile = async (data, token) => {
     const res = await fetch(api + '/users/profile', config)
       .then((res) => res.json())
       .catch((err) => err)
+
     return res
   } catch (error) {
     console.log(error)
   }
 }
 
-
 // Update user details
 const updateProfile = async (data, token) => {
-  const config = requestConfig("PUT", data, token, true);
+  const config = requestConfig('PUT', data, token, true)
 
   try {
-    const res = await fetch(api + "/users/", config)
+    const res = await fetch(api + '/users/', config)
       .then((res) => res.json())
-      .catch((err) => err);
+      .catch((err) => err)
 
-    return res;
+    return res
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
+// Get user details
+const getUserDetails = async (id) => {
+  const config = requestConfig('GET')
+
+  try {
+    const res = await fetch(api + '/users/' + id, config)
+      .then((res) => res.json())
+      .catch((err) => err)
+
+    return res
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const userService = {
   profile,
   updateProfile,
+  getUserDetails,
 }
 
 export default userService
