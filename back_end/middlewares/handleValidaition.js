c7223acad4339e0ffcc796817ac575c363391c50
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-07 20:21:36 -0300

    :construction:
    Atualizando 07/03/2023

--- back_end/middlewares/handleValidaition.js
+++ back_end/middlewares/handleValidaition.js
@@ -1,21 +1,22 @@
 //função de validação que será utilizada nas rotas
-const { validationResult } = require('express-validator')
+const { validationResult } = require("express-validator");
 
 const validate = (req, res, next) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
 
-  //se esta vazio, não tem erro continua a requisição
   if (errors.isEmpty()) {
-    return next()
+    return next();
   }
 
-  // agora de ele não esta vazio ele tem erro (erros extratidos da requisição)
-  const extractedErrors = []
-  errors.array().map((err) => extractedErrors.push(err.msg))
+  const extractedErrors = [];
+
+  errors.array().map((err) => extractedErrors.push(err.msg));
 
   return res.status(422).json({
     errors: extractedErrors,
-  })
-}
+  });
+};
 
-module.exports = validate
+module.exports = validate;
+
+
