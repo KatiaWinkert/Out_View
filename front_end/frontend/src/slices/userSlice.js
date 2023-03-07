1ed30df0a6739ce97e2055f03c1921ff2e7dc059
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-07 20:35:31 -0300

    :sparkles: userSlice
    Atualizando 07/03/2023

--- front_end/frontend/src/slices/userSlice.js
+++ front_end/frontend/src/slices/userSlice.js
@@ -9,31 +9,49 @@ const initialState = {
   message: null,
 }
 
-//funções: get user details
-
+// Get user details, for edit data
 export const profile = createAsyncThunk(
   'user/profile',
-  async (user, thunkAPI) => {
-    const token = thunkAPI.getState.auth.user.token
-
-    const data = await userService.profile(user, token)
-
-    return data
-  }
-)
-
-//atualizar daddos do usuario : update user details
-export const updateProfile = createAsyncThunk(
-  'user/update',
   async (user, thunkAPI) => {
     const token = thunkAPI.getState().auth.user.token
 
     const data = await userService.profile(user, token)
 
-    //check error
-    if (data.error) {
+    console.log(data)
+
+    return data
+  }
+)
+
+// Update user details
+export const updateProfile = createAsyncThunk(
+  'user/update',
+  async (user, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+
+    const data = await userService.updateProfile(user, token)
+
+    // Check for errors
+    if (data.errors) {
       return thunkAPI.rejectWithValue(data.errors[0])
     }
+
+    console.log(data)
+
+    return data
+  }
+)
+
+// Get user details
+export const getUserDetails = createAsyncThunk(
+  'user/get',
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+
+    const data = await userService.getUserDetails(id, token)
+
+    console.log(data)
+
     return data
   }
 )
@@ -67,12 +85,22 @@ export const userSlice = createSlice({
         state.success = true
         state.error = null
         state.user = action.payload
-        state.message = 'Usuário atualizando com sucesso!'
+        state.message = 'Usuário atualizado com sucesso!'
       })
       .addCase(updateProfile.rejected, (state, action) => {
         state.loading = false
         state.error = action.payload
         state.user = null
+      })
+      .addCase(getUserDetails.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(getUserDetails.fulfilled, (state, action) => {
+        state.loading = false
+        state.success = true
+        state.error = null
+        state.user = action.payload
       })
   },
 })
