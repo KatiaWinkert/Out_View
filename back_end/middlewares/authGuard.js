c1ce0349b99be1349b46d0299d71e1e7236396e5
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-03-07 20:20:53 -0300

    :construction: Back End
    Atualizando 07/03/2023

--- back_end/middlewares/authGuard.js
+++ back_end/middlewares/authGuard.js
@@ -8,20 +8,18 @@ const authGuard = async (req, res, next) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
-  //Bearer yy32156y12156y46545d546
-  //checando se o headers tem um token - check if header has a token:
-
+  // Check if header has a token
   if (!token) return res.status(401).json({ errors: ['Acesso negado!'] })
 
-  //checando se o token é valido - check toke is valid
+  // Check if token is valid
   try {
     const verified = jwt.verify(token, jwtSecret)
 
     req.user = await User.findById(verified.id).select('-password')
-    
+
     next()
   } catch (err) {
-    res.status(400).json({ errors: [{ msg: "token inválido."}] })
+    res.status(400).json({ errors: ['O Token é inválido!'] })
   }
 }
 
